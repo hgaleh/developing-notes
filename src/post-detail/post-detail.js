3f6cff74e2d46aa797b74429f8f05587cc7cc5a4
Hojjat <h.galeh@gmail.com>
2021-01-16 16:42:40 +0330

    blog style done

--- src/post-detail/post-detail.js
+++ src/post-detail/post-detail.js
@@ -2,6 +2,30 @@ import styles from './post-detail.module.scss';
 
 export const PostDetail = () => {
     return <div className={styles.postDetail}>
-        Content
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+                <img src="./profile-photo.jpeg" alt="rwparrish profile"></img>
+            </div>
+            <div className={styles.headerRight}>
+                <div className={styles.writerName}>
+                    rwparrish
+                </div>
+                <div className={styles.publishDate}>
+                    Jan 9
+                </div>
+            </div>
+        </div>
+        <div className={styles.body}>
+            <h3 className={styles.title}>
+                <a href="#">
+                    Changing sate before saving to the database in Rails
+                </a>
+            </h3>
+            <p className={styles.brief}>
+                Changing sate before saving to the database in RailsChanging
+                sate before saving to the database in Rails
+                Changing sate before saving to the database in Rails
+            </p>
+        </div>
     </div>;
 }

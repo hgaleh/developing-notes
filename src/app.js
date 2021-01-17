cc5137063f55c3bb981f7c2f653d4a435b2f65c3
Hojjat <h.galeh@gmail.com>
2021-01-17 14:26:26 +0330

    build errors and neting errors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import './app.module.scss';
 import { Footer } from './component/footer/footer';
 import { Home } from './page/home/home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import { PostDetail } from './page/post-detail/post-detail';
 
 
--- src/page/post-detail/post-detail.js
+++ src/page/post-detail/post-detail.js
@@ -7,8 +7,8 @@ export const PostDetail = () => {
     const { postId } = useParams();
     return <div className={styles.detailContainer}>
         <article className={styles.detailContent}>
+            {postId}
             In this blog, I will cover how to create a 'bcrypt" module for use in a Ruby on Rails project.
-
             Bcrypt uses the MD5 algorithm and adds in a salt to it in order to hash passwords. This is important because as developers we do not want passwords to be stored as regular old strings. Believe it or not, this used to be the case. However, this is no longer safe and NOT considered to be best practice.
             In this blog, I will cover how to create a 'bcrypt" module for use in a Ruby on Rails project. Bcrypt uses the MD5 algorithm and adds in a salt to it in order to hash passwords. This is important because as developers we do not want passwords to be stored as regular old strings. Believe it or not, this used to be the case. However, this is no longer safe and NOT considered to be best practice. The code below is a module. I have called this module "Crud". The methods in this module can be easily called on from other classes or files:
             In this blog, I will cover how to create a 'bcrypt" module for use in a Ruby on Rails project. Bcrypt uses the MD5 algorithm and adds in a salt to it in order to hash passwords. This is important because as developers we do not want passwords to be stored as regular old strings. Believe it or not, this used to be the case. However, this is no longer safe and NOT considered to be best practice. The code below is a module. I have called this module "Crud". The methods in this module can be easily called on from other classes or files:
@@ -37,30 +37,30 @@ export const PostDetail = () => {
                 </HoverableLink>
                 <div>Frontend Developer at NICICO</div>
                 <div>
-                    <p>
+                    <div className={styles.detail}>
                         <div>WORK</div>
                         <div>
                             Frontend Web Developer at NICICO
                         </div>
-                    </p>
-                    <p>
+                    </div>
+                    <div className={styles.detail}>
                         <div>LOCATION</div>
                         <div>
                             Tehran, Iran
                         </div>
-                    </p>
-                    <p>
+                    </div>
+                    <div className={styles.detail}>
                         <div>EDUCATION</div>
                         <div>
                             Master of Computer Science
                         </div>
-                    </p>
-                    <p>
+                    </div>
+                    <div className={styles.detail}>
                         <div>JOINED</div>
                         <div>
                             Jan 14, 2020
                         </div>
-                    </p>
+                    </div>
                 </div>
             </div>
         </aside>

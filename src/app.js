1ed5f8f9ba6804eeed1542c85202e188d25f4fa0
Hojjat <h.galeh@gmail.com>
2021-01-17 13:58:36 +0330

    build error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-import styles from './app.module.scss';
 import { Footer } from './component/footer/footer';
 import { Home } from './page/home/home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { PostDetail } from './page/post-detail/post-detail';
 
 

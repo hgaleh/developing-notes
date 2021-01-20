56c620486c3fcf6e51ce75460e42446a9174086e
Hojjat <h.galeh@gmail.com>
2021-01-20 23:19:18 +0330

    hamburger ok

--- src/component/hamburger/hamburger.js
+++ src/component/hamburger/hamburger.js
@@ -2,6 +2,14 @@ import { HamburgerIcon } from '../svg/hamburger-icon';
 import styles from './hamburger.module.scss';
 import React from 'react';
 import { RoundButton } from '../round-button/round-button';
+import { CloseIcon } from '../svg/close-icon';
+import { HoverableLink } from '../hoverable/hoverable-link';
+import { HomeIcon } from '../svg/home';
+import { ReadingListIcon } from '../svg/reading-list-icon';
+import { ListingsIcon } from '../svg/listings-icon';
+import { PodcastsIcon } from '../svg/podcasts-icon';
+import { VideosIcon } from '../svg/videsos-icon';
+import { TagsIcon } from '../svg/tags-icon';
 
 export class Hamburger extends React.Component {
 
@@ -27,7 +35,28 @@ export class Hamburger extends React.Component {
                     <HamburgerIcon onClick={this.hamburgerClicked}></HamburgerIcon>
                 </RoundButton>
                 <div style={{display: (this.state && this.state.show) ? 'block' : 'none'}} className={styles.hamburgerContainer}>
-                    <div className={styles.menu}></div>
+                    <div className={styles.menu}>
+                        <div className={styles.header}>
+                            <h2>
+                                Dev Comunity
+                            </h2>
+                            <RoundButton onClick={this.clickedOutside}>
+                                <CloseIcon></CloseIcon>
+                            </RoundButton>
+                        </div>
+
+                        <div className={styles.body}>
+                            <ul>
+                                <li><HoverableLink href=''><i><HomeIcon></HomeIcon></i>Home</HoverableLink></li>
+                                <li><HoverableLink href=''><i><ReadingListIcon></ReadingListIcon></i>Reading List</HoverableLink></li>
+                                <li><HoverableLink href=''><i><ListingsIcon></ListingsIcon></i>Listings</HoverableLink></li>
+                                <li><HoverableLink href=''><i><PodcastsIcon></PodcastsIcon></i>Podcasts</HoverableLink></li>
+                                <li><HoverableLink href=''><i><VideosIcon></VideosIcon></i>Videos</HoverableLink></li>
+                                <li><HoverableLink href=''><i><TagsIcon></TagsIcon></i>Tags</HoverableLink></li>
+                                <li><HoverableLink href=''>More...</HoverableLink></li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className={styles.out} onClick={this.clickedOutside}></div>
                 </div>
             </div>
---
+++ src/component/svg/close-icon.js
@@ -0,0 +1,10 @@
+import styles from './svg.module.scss';
+
+export const CloseIcon = () => {
+    return <svg xmlns="http://www.w3.org/2000/svg" className={styles.colorOfParent} width="24" height="24" viewBox="0 0 24 24" role="img"
+     aria-labelledby="alpl1iluim9s504gqs4eh3hdrq6tfv88">
+         <title id="alpl1iluim9s504gqs4eh3hdrq6tfv88">Close</title>
+         <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z">
+         </path>
+    </svg>
+}
